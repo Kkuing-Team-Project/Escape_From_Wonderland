b2c63d0a930c3b249e258744e672b67664f26abb
yeop._.04 <HwakKillhan@gmail.com>
2023-06-13 03:23:43 +0800

    f

--- JiSeong/G.P.ex2/Assets/Script/Alice.cs
+++ JiSeong/G.P.ex2/Assets/Script/Alice.cs
@@ -5,28 +5,45 @@ using UnityEngine;
 public class Alice : MonoBehaviour
 {
     private Animator animator;
+    public Transform player;
+    public float followDistance = 5f;
+    public float attackDistance = 2f;
+    public float movementSpeed = 5f;
+
     // Start is called before the first frame update
     IEnumerator Start()
     {
         animator = GetComponent<Animator>();
-        while(true)
+
+        while (true)
         {
-            yield return new WaitForSeconds(3);
+            float attackDelay = Random.Range(3f, 5f); // 3초에서 5초 사이의 랜덤 딜레이
+            yield return new WaitForSeconds(attackDelay);
+
+            // 플레이어와의 거리를 0에서 2 사이로 줄임
+            float targetX = player.position.x + Random.Range(-attackDistance, attackDistance);
+            Vector3 targetPosition = new Vector3(targetX, transform.position.y, transform.position.z);
+
+            StartCoroutine(MoveToTarget(targetPosition));
 
             animator.SetInteger("AttackIndex", Random.Range(0, 2));
             animator.SetTrigger("Attack");
         }
     }
 
-    // Update is called once per frame
-    private void OnCollisionEnter2D(Collision2D collision) 
+    private void Update()
     {
-        if(collision.gameObject.name == "Player") //내 프로젝트 내 플레이어 이름이 move라 맞춰서 수정하세요
+        float targetX = player.position.x - followDistance;
+        Vector3 targetPosition = new Vector3(targetX, transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime);
+    }
+
+    IEnumerator MoveToTarget(Vector3 target)
+    {
+        while (transform.position != target)
         {
-            Destroy(collision.gameObject);
-            Debug.Log("Destroy");
+            transform.position = Vector3.MoveTowards(transform.position, target, movementSpeed * Time.deltaTime);
+            yield return null;
         }
     }
-    
-
 }
