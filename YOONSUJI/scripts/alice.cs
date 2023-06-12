e46bcc5358909208bfb7da6b1231f3f8d5f83a90
suji_sz <103244178+Yoonsuji@users.noreply.github.com>
2023-06-12 17:14:53 +0900

    앨리스 랜덤 공격까지 Alice.cs

--- YOONSUJI/scripts/alice.cs
+++ YOONSUJI/scripts/alice.cs
@@ -2,22 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class alice : MonoBehaviour
+public class Alice : MonoBehaviour
 {
+    private Animator animator;
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
-        
+        animator = GetComponent<Animator>();
+        while(true)
+        {
+            yield return new WaitForSeconds(3);
+
+            animator.SetInteger("AttackIndex", Random.Range(0, 2));
+            animator.SetTrigger("Attack");
+        }
     }
 
     // Update is called once per frame
     private void OnCollisionEnter2D(Collision2D collision) 
     {
-        if(collision.gameObject.name == "move") //move가 플레이어임 각자 씬에 맞춰서 수정
+        if(collision.gameObject.name == "move") 
         {
             Destroy(collision.gameObject);
             Debug.Log("Destroy");
         }
     }
+    
 
 }
