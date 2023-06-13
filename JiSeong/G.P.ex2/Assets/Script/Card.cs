ece086a1e1aca452965b8785c386d0a1bfa46ebf
yoonjiseang <imzzang3u@naver.com>
2023-06-13 18:18:44 +0900

    토끼 터지는거 구현

--- JiSeong/G.P.ex2/Assets/Script/Card.cs
+++ JiSeong/G.P.ex2/Assets/Script/Card.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    public Effact effact;
-    
+    public GameObject effectPrefab; // 이펙트 프리팹을 할당하기 위한 변수
+
     private void OnTriggerEnter2D(Collider2D collision)
     {   
         if (collision.CompareTag("Rabbit"))
         {
             Player.instance.KillCountNull++;
+
             Destroy(collision.gameObject);
             Destroy(gameObject);
+
+            // 이펙트를 생성하고 충돌 지점에 배치
+            Instantiate(effectPrefab, transform.position, Quaternion.identity);
+
+            // 이펙트 재생이 끝나면 제거
+            Destroy(effectPrefab, 1f);
         }
     }
 
