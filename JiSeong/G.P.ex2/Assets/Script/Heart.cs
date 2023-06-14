fb0b8942d36e4f35bb9d09dc5c3a50edaa04d9cd
yoonjiseang <imzzang3u@naver.com>
2023-06-14 15:27:33 +0900

    하트 구현1

--- JiSeong/G.P.ex2/Assets/Script/Heart.cs
+++ JiSeong/G.P.ex2/Assets/Script/Heart.cs
@@ -6,20 +6,32 @@ using UnityEngine.UI; //Text, Image 등의UI관련 변수 등을 사용할수 있게됩니다
 public class Heart : MonoBehaviour
 {
     Image heart3; //기존에 존제하는 이미지
-    public Image heart2; //바뀌어질 이미지
-    public Image heart1;
+    public Sprite HpImage2;
+    public Sprite HpImage1;
+    public Sprite HpImage;
+    public Player player;
+    public Image hpUi;
 
     public void ChangeImage()
     {
         
-        heart3 = GetComponent<Image>();
     }
     private void Update()
     {
-        if (GameObject.Find("Player").GetComponent<Player>().PlayerHp == 3)
+        switch (player.PlayerHp)
         {
-
+            case 3:
+                hpUi.sprite = HpImage;
+                break;
+            case 2:
+                hpUi.sprite = HpImage1;
+                break;
+            case 1:
+                hpUi.sprite = HpImage2;
+                break;
+            default:
+                // PlayerHp가 3, 2, 1이 아닌 경우에 대한 처리를 여기에 추가할 수 있습니다.
+                break;
         }
     }
-
 }
